2c2a16f6388ae02a6e7ae71b11019e0d3c8a67fd
Naima Africa Francis <naima.africa@gmail.com>
2020-08-14 13:29:56 -0400

    Set up ranks and suits.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BlackJackCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var deck = new List<string>();
+            var suits = new List<string>() { "Hearts", "Diamonds", "Spades", "Clubs" };
+            var ranks = new List<string>() { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+
+        }
     }
-  }
 }
